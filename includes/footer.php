294b571f002a640b26825a3bb2a82400afa64641
Tobias Daugaard <tobias.daugaard@hotmail.com>
2021-02-02 11:19:20 +0100

    git init

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+<div class="footer">
+    <div>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+    </div>
+    <div>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+    </div>
+    <div>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+    </div>
+</div>
---
+++ includes/navigation.php
@@ -0,0 +1,6 @@
+<div class="navigation">
+    <a href="index.php">Forside</a>
+    <a href="#">Om os</a>
+    <a href="contact.php">Kontakt</a>
+    <a href="#">Ny side</a>
+</div>
